b9b178b3fff4cb00f5c5729b61c9c6d314de2ce1
Jordan Stella <jordanjstella@gmail.com>
2026-06-02 10:49:01 -0700

    Revert "Create a new sidebar option underneath History in the left rail. Make it"
    This reverts commit ada5a4a8eeb31a1e30f41a2a8adf35000471d765.

--- src/routes/demo-calendar.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { DemoCalendar } from '@/app/demo-calendar/DemoCalendar'
-
-export const Route = createFileRoute('/demo-calendar')({
-  component: DemoCalendar,
-})
